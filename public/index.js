dcd9a9f40caf2517ed58ec4bd2918d1364f3c50e
Francisco Siracusa <francisco.siracusa@gmail.com>
2021-08-24 20:11:54 -0300

    getItems fix

--- public/index.js
+++ public/index.js
@@ -1,111 +1,131 @@
+const deleteItem = (id) => {
+	console.log(id);
+}
+
 const getItems = () => {
   fetch("/getItem")
-  	.then((res) => res.json())
+    .then((res) => res.json())
     .then((data) => {
-			const list = document.getElementById('item-list');
-			if (list.hasChildNodes()) {
-				const itemlist = document.createElement('li');
-				const deletebtn = document.createElement('button');
-				const editbtn = document.createElement('button');
-				deletebtn.textContent = 'Borrar';
-				editbtn.textContent = 'Editar'
-				itemlist.textContent = data[data.length-1].description;
-				itemlist.appendChild(deletebtn);
-				itemlist.appendChild(editbtn);
-				list.appendChild(itemlist);
-			} else {
-				const fragment = document.createDocumentFragment();
-				data.forEach(item => {
-					const itemlist = document.createElement('li');
-					itemlist.textContent = item.description;
-					fragment.appendChild(itemlist);
-				});
-				list.appendChild(fragment);
-			}
-		});
+			const itemContainer = document.getElementById("item-container");
+      if (itemContainer.hasChildNodes()) {
+        const div = document.createElement("div");
+				div.className = 'item';
+				div.setAttribute('data-id',data.length);
+        const p = document.createElement("p");
+        const deletebtn = document.createElement("button");
+        const editbtn = document.createElement("button");
+        deletebtn.textContent = "Borrar";
+        editbtn.textContent = "Editar";
+        p.textContent = data[data.length - 1].description;
+				deletebtn.addEventListener('click', () => deleteItem(div.dataset.id));
+        div.appendChild(p);
+        div.appendChild(deletebtn);
+        div.appendChild(editbtn);
+        itemContainer.appendChild(div);
+      } else {
+        const fragment = document.createDocumentFragment();
+        data.forEach((item,index) => {
+          const div = document.createElement("div");
+					div.className = 'item';
+					div.id = 'item';
+					div.setAttribute('data-id',index+1);
+          const p = document.createElement("p");
+          const deletebtn = document.createElement("button");
+          const editbtn = document.createElement("button");
+          deletebtn.textContent = "Borrar";
+          editbtn.textContent = "Editar";
+          p.textContent = item.description;
+					deletebtn.addEventListener('click', () => deleteItem(div.dataset.id));
+          div.appendChild(p);
+          div.appendChild(deletebtn);
+          div.appendChild(editbtn);
+          fragment.appendChild(div);
+        });
+        itemContainer.appendChild(fragment);
+      }
+			
+    });
 };
 
 const getFolders = () => {
   fetch("/getFolder")
-  	.then((res) => res.json())
+    .then((res) => res.json())
     .then((data) => {
-			const list = document.getElementById('folder-list');
-			if (list.hasChildNodes()) {
-				const folderlist = document.createElement('li');
-				folderlist.textContent = data[data.length-1].name;
-				list.appendChild(folderlist);
-			} else {
-				const fragment = document.createDocumentFragment();
-				data.forEach(item => {
-					const folderlist = document.createElement('li');
-					folderlist.textContent = item.name;
-					fragment.appendChild(folderlist);
-				});
-				list.appendChild(fragment);
-			}
-		});
+      const list = document.getElementById("folder-list");
+      if (list.hasChildNodes()) {
+        const folderlist = document.createElement("li");
+        folderlist.textContent = data[data.length - 1].name;
+        list.appendChild(folderlist);
+      } else {
+        const fragment = document.createDocumentFragment();
+        data.forEach((item) => {
+          const folderlist = document.createElement("li");
+          folderlist.textContent = item.name;
+          fragment.appendChild(folderlist);
+        });
+        list.appendChild(fragment);
+      }
+    });
 };
 
-const createItem = () =>{
-	const form = document.createElement('form');
-  form.id = 'form';
-  const formContainer = document.getElementById('form-container');
-	formContainer.appendChild(form);
-  const input = document.createElement('input');
-  const btn = document.createElement('button');
-	input.id = 'description';
-  input.name = 'description';
-  input.placeholder = 'Insertar Tarea';
-  input.pattern = '^[a-zA-Z ñÑ]*$';
+const createItem = () => {
+  const form = document.createElement("form");
+  form.id = "form";
+  const formContainer = document.getElementById("form-container");
+  formContainer.appendChild(form);
+  const input = document.createElement("input");
+  const btn = document.createElement("button");
+  input.id = "description";
+  input.name = "description";
+  input.placeholder = "Insertar Tarea";
+  input.pattern = "^[a-zA-Z ñÑ]*$";
   input.required = true;
   input.maxLength = 50;
-	btn.textContent = 'crear';
-	form.appendChild(input);
+  btn.textContent = "crear";
+  form.appendChild(input);
   form.appendChild(btn);
-	btn.addEventListener('click', (e) =>{
-		e.preventDefault();
-		fetch(`/createItem?description=${input.value}`,{
-			method: 'POST',
-		})
-			.then(res => getItems())
-	})
-}
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch(`/createItem?description=${input.value}`, {
+      method: "POST",
+    }).then((res) => getItems());
+  });
+};
 
-const createFolder = () =>{
-	const form = document.createElement('form');
-  form.id = 'form';
-  const formContainer = document.getElementById('form-container');
-	formContainer.appendChild(form);
-  const input = document.createElement('input');
-  const btn = document.createElement('button');
-	input.id = 'name';
-  input.name = 'name';
-  input.placeholder = 'Insertar Nombre de Carpeta';
-  input.pattern = '^[a-zA-Z ñÑ]*$';
+const createFolder = () => {
+  const form = document.createElement("form");
+  form.id = "form";
+  const formContainer = document.getElementById("form-container");
+  formContainer.appendChild(form);
+  const input = document.createElement("input");
+  const btn = document.createElement("button");
+  input.id = "name";
+  input.name = "name";
+  input.placeholder = "Insertar Nombre de Carpeta";
+  input.pattern = "^[a-zA-Z ñÑ]*$";
   input.required = true;
   input.maxLength = 50;
-	btn.textContent = 'crear';
-	form.appendChild(input);
+  btn.textContent = "crear";
+  form.appendChild(input);
   form.appendChild(btn);
-	btn.addEventListener('click', (e) =>{
-		e.preventDefault();
-		fetch(`/createItem?name=${input.value}`,{
-			method: 'POST',
-		})
-			.then(res => getFolders())
-	})
-}
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch(`/createItem?name=${input.value}`, {
+      method: "POST",
+    }).then((res) => getFolders());
+  });
+};
 
 window.addEventListener("load", () => {
-	const btnItem = document.createElement('button');
-	const btnFolder = document.createElement('button');
-	const folderCotainer = document.getElementById('folder-container');
-	const itemContainer = document.getElementById('item-container');
-	btnFolder.textContent = 'Desplegar Carpetas'
-	btnItem.textContent = 'Desplegar Items'
-	folderCotainer.appendChild(btnFolder);
-	itemContainer.appendChild(btnItem);
-	btnItem.addEventListener('click' , () => getItems());
-	btnFolder.addEventListener('click', () => getFolders());
-	createItem();
+  const btnItem = document.createElement("button");
+  const btnFolder = document.createElement("button");
+  const folderCotainer = document.getElementById("folder-container");
+  const itemContainer = document.getElementById("item-container");
+  btnFolder.textContent = "Desplegar Carpetas";
+  btnItem.textContent = "Desplegar Items";
+  document.body.appendChild(btnFolder);
+  document.body.appendChild(btnItem);
+  btnItem.addEventListener("click", () => getItems());
+  btnFolder.addEventListener("click", () => getFolders());
+  createItem();
 });
